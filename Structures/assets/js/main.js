b12a63794cc82b1369d4dcbaaf1df83140f08476
dickyadrianto <dickyadrianto@gmail.com>
2022-05-12 12:31:42 +0800

    Big Update

--- Structures/assets/js/main.js
+++ Structures/assets/js/main.js
@@ -5,11 +5,14 @@
 const navbarToggle = document.getElementById('navbar-toggle');
 const navList = document.getElementById('nav-list');
 
-
 navbarToggle.addEventListener('click', () =>{
     navList.classList.toggle('nav-active');
     navbarToggle.classList.toggle("change");
 })
+
+window.onload = function (){
+    autoSlide()
+}
 
 function sendSuggestionMail() {
     const name = document.getElementById('name-input').value;
@@ -23,4 +26,22 @@ function sendSuggestionMail() {
 function sendEmail()
 {
     window.location = "mailto:kadek0086@gmail.com?subject=Yicambalii Order&body=Hello! I want to order Equipments/Services";
+}
+
+var i = 0;
+function slide(){
+    if(i > 2)
+        i=0;
+    document.getElementById('cont'+(i%3)).innerHTML = "Lorem Ipsum Dolor Sit Amet";
+    document.getElementById('cont'+(i%3)).href = "TipsTricks.html";
+    document.getElementById('cont'+((i+1)%3)).innerHTML = "Get a Good Camera Bag";
+    document.getElementById('cont'+((i+1)%3)).href = "TipsTricks.html#GGcb";
+    document.getElementById('cont'+((i+2)%3)).innerHTML = "Take The Batteries Out";
+    document.getElementById('cont'+((i+2)%3)).href = "TipsTricks.html#TTbo";
+    i++;
+}
+
+function autoSlide() {
+    slide();
+    setTimeout(autoSlide,3000);
 }
